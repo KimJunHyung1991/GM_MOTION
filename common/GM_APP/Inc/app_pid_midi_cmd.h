bad52a40a80787653079f326269ac4d5ab1ef5a5
KimJunHyung1191 <pcz365@naver.com>
2021-04-13 13:44:52 +0900

    add

--- common/GM_APP/Inc/app_pid_midi_cmd.h
+++ common/GM_APP/Inc/app_pid_midi_cmd.h
@@ -7,7 +7,7 @@ void app_rx_midi_sub_pid_adc_rsp(uint8_t num, prtc_header_t *pPh, uint8_t *pData
 void app_rx_midi_sub_pid_adc_rqt(uint8_t num, prtc_header_t *pPh, uint8_t *pData);
 void app_tx_midi_sub_pid_adc_ctl(uint8_t num, uint8_t priority, uint8_t souce_id, uint8_t target_id, uint8_t sub_id, uint8_t id, uint16_t adc_val);
 void app_tx_midi_sub_pid_adc_rsp(uint8_t num, uint8_t priority, uint8_t souce_id, uint8_t target_id, uint8_t sub_id, uint8_t id, uint16_t adc_val);
-void app_tx_midi_sub_pid_adc_rqt(uint8_t num, uint8_t priority, uint8_t souce_id, uint8_t target_id, uint8_t sub_id);
+void app_tx_midi_sub_pid_adc_rqt(uint8_t num, uint8_t priority, uint8_t souce_id, uint8_t target_id, uint8_t sub_id, uint8_t id);
 /**********************************************************************************/
 /**********************************************************************************/
 void app_rx_midi_sub_pid_button_ctl(uint8_t num, prtc_header_t *pPh, uint8_t *pData);
