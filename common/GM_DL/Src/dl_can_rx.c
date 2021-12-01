151548a6253c932f4f7b88d9a316a6257fa8f2d9
KimJunHyung1191 <pcz365@naver.com>
2021-12-01 14:36:42 +0900

    add

--- common/GM_DL/Src/dl_can_rx.c
+++ common/GM_DL/Src/dl_can_rx.c
@@ -127,7 +127,7 @@ void proc_can_rx(void)
 	for(int i = 0; i < can_init.cnt; i++){
 		if(can_rx_ring_buff[i].head != can_rx_ring_buff[i].tail){
 			prtc_header_t *pPh = (prtc_header_t *)&can_rx_ring_buff[i].can_header[can_rx_ring_buff[i].tail];
-			if((pPh->target_id == my_can_id || pPh->target_id == CAN_ID_BROAD_CAST) && (pPh->souce_sub_id == my_can_sub_id || pPh->souce_sub_id == CAN_SUB_ID_BROAD_CAST)){
+			if((pPh->target_id == my_can_id || pPh->target_id == CAN_ID_BROAD_CAST) && (pPh->target_sub_id == my_can_sub_id || pPh->target_sub_id == CAN_SUB_ID_BROAD_CAST)){
 				gm_motion_RX_LED_ON(i);//210218 shs//210430kjh
 				net_phd_pid(i, &can_rx_ring_buff[i].can_header[can_rx_ring_buff[i].tail], (uint8_t *)&can_rx_ring_buff[i].data[can_rx_ring_buff[i].tail]);
 			}
