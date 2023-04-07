de088dfcb00068c1c15c8ac5ee1171e345c9049a
KimJunHyung1991 <pcz365@naver.com>
2023-04-07 17:39:43 +0900

    2.1.6V
    #define MOVE_SENSOR_DATA_ABSOLUTE_MODE		0
    #define MOVE_SENSOR_DATA_NOMAL_MODE			1
    
    변경

--- common/GM_PRTC/Inc/prtc_data_pid_init.h
+++ common/GM_PRTC/Inc/prtc_data_pid_init.h
@@ -73,8 +73,8 @@ typedef struct{
 }prtc_data_rsp_init_absolute_battery_t;
 /***********************************************************INIT_SUB_PID_ABSOLUTE_BATTERY**********************************************************/
 /***********************************************************INIT_SUB_PID_MOVE_SENSOR**********************************************************/
-#define MOVE_SENSOR_DATA_NOMAL_MODE	0
-#define MOVE_SENSOR_DATA_ABSOLUTE_MODE	1
+#define MOVE_SENSOR_DATA_ABSOLUTE_MODE		0
+#define MOVE_SENSOR_DATA_NOMAL_MODE			1
 
 typedef struct{
 	uint8_t mode;
