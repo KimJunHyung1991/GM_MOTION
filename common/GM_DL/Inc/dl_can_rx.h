ffdce528aae58d79abb1405365b955e355ecb65b
minimirror1 <minimirror1@gmail.com>
2023-11-10 10:59:39 +0900

    2.4.4_dl_can_rx.h 에 ST HAL Driver 중 HAL_CAN_RxFifo0MsgPendingCallback 함수를 그대로 전달하는 ByPass 함수를 추가. MRS 와 다른 프로토콜로 통신하는 CAN RX사용시 ByPass함수를 활용한다.

--- common/GM_DL/Inc/dl_can_rx.h
+++ common/GM_DL/Inc/dl_can_rx.h
@@ -4,6 +4,7 @@
 extern can_q_buff_t can_tx_ring_buff[CAN_CNT];
 
 void gm_motion_TX_LED_init(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState ledOnState);//210218 shs
+void HAL_CAN_RxFifo0MsgPendingCallback_ByPass(CAN_HandleTypeDef *hcan);//231110 shs
 
 void proc_can_rx(void);
 void proc_can_rx_test(void);
